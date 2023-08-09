cf8480bca582011de282d7d5df9217bbc2f3e761
choudharyumar <choudharyumar948@gmail.com>
2023-08-09 12:12:46 +0500

    1

---
+++ tut55.js
@@ -0,0 +1,91 @@
+// working of for loop
+
+// let i=0;
+// for(i;i<=3;i++)
+// {
+//     console.log(i)
+// }
+
+
+
+// working of for loop for iteration of array
+
+// let frnds=["umar","khizar","ali"]
+// for(let i=0;i<frnds.length;i++)
+
+
+// this loop is working for (length-1)
+// And if we use i<=frnds.length than it work for one more
+
+// {
+//     console.log("hello my dear "+frnds[i])
+// }
+
+
+
+
+// working of for each
+
+
+// let frnds=["umar","khizar","ali"]
+// frnds.forEach(function abc(element)
+// {
+//     console.log(element)
+// })
+
+
+
+
+// working of for of loop
+
+// let frnds=["umar","khizar","ali"]
+
+// for(element of frnds)
+// {
+//     console.log(element)
+// }
+
+
+
+// working of for in loop
+
+// let obj={
+//     name:"umar",
+//     age :34,
+//     class:"seven"
+// }
+// for(key in obj)
+// {
+//     // console.log(`the ${key} of obj is ${obj[key]}`)
+//      console.log(key+":"+obj[key])
+// }
+
+
+
+
+// working of while loop
+
+// agr condition ghalat ho gi to nai chaly ga
+
+// let j=0;
+// while (j<4) {
+//     console.log(j+" is less than 4")
+//     j++;
+// }
+
+
+// working of do while loop
+
+// it also work as while loop but always run for 1 time if condition was wrong
+
+
+// agr condition taek ho gi to phr bilkul utni bar hi chly ga jitni bar taek hai
+
+let i=9;
+ do{
+    console.log(i+" is less than 4")
+    i++;
+}
+while (i<4)
+
+// 
