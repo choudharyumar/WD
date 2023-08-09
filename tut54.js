cac2410415e37e905f4bd5658948d46eb0d505ef
choudharyumar <choudharyumar948@gmail.com>
2023-08-09 08:42:23 +0500

    1

---
+++ tut54.js
@@ -0,0 +1,35 @@
+// working of alert
+// it is a function where it shows a msg to user
+// it does not return anything
+
+// alert("it is the call of alert")
+
+// working of prompt
+// let name = prompt("what is your name?", "Brother");
+
+// jo bi hm prompt ma value dain gy to wohi console k andr show ho gi
+
+// console.log(name);
+
+
+
+// working of confirm
+// let dpost = confirm("Do you really want to delete this post?");
+// console.log(dpost)
+
+// working of if else in confirm
+// if (dpost) {
+//   console.log("your post has been deleted successfully");
+// } else {
+//   console.log("your post has not been deleted ");
+// }
+
+// quiz
+let age = prompt("What is your age?");
+if (age >= 18) {
+  console.log("you are valid for this entery");
+}
+else
+{
+    console.log("you are not valid for this entery")
+}
